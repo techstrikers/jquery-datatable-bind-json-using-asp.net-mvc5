d0863cd403606a06b14d84771dcf7e4b33465968
techstrikers <ajaysaptaputre@techstrikers.com>
2016-02-06 18:29:11 +0530

    CheckIn

---
+++ JQueryDataTableMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using JQueryDataTableMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryDataTableMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        JQueryDataTableDBContext db = new JQueryDataTableDBContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Employee/getEmployees
+        [HttpGet]
+        public JsonResult getEmployees()
+        {
+            var jsonData = new
+            {
+                data = from emp in db.Employees.ToList() select emp
+            };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+        } 
+	}
+}
